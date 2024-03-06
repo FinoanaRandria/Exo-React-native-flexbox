ebb059e7427cd6e2d12b256067bcee77c00e936a
finoana <110087713+FinoanaRandria@users.noreply.github.com>
2024-03-06 21:54:31 +0300

    devoir flexbox react native

--- App.js
+++ App.js
@@ -1,27 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.main_container}>
-      <Text style={styles.text_color}>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>
       <StatusBar style="auto" />
+      <View style={{ flexDirection: "row", height: 50, width: 50, marginTop: 25 }}>
+        <View
+          style={{
+            backgroundColor: "blue",
+            justifyContent: "center",
+            width: 50,
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "purple",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 100,
+          }}
+        >
+          <Text>2</Text>
+        </View>
+
+        <View
+          style={{
+            backgroundColor: "yellow",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 250,
+          }}
+        >
+          <Text>3</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "red",
+          width: 400,
+          height: 50,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>4</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          width: 400,
+          height: 50,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>5</Text>
+      </View>
+
+      <View style={{ flexDirection: "row" ,flex:1 }}>
+        <View style={{backgroundColor:'white', width:200,justifyContent:'center', alignItems:'center'}}>
+          <Text >6</Text>
+        </View>
+
+        <View style={{backgroundColor:'blue',width:200,justifyContent:'center', alignItem:'center'}}>
+          <Text>7</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles =StyleSheet.create({
-     main_container:{
-        backgroundColor:"white",
-        display:"flex",
-        flex:1,
-        alignItems:'center',
-        justifyContent:"center",
-        borderColor:"black"
-     },
-     text_color:{
-      color:"green",
-      borderColor:"black"
-     }
-
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
